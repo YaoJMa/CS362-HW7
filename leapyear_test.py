f2e4fb113c97ae0619b47d68e2ed97e0689f621e
Yao Ma <mayao@flip1.engr.oregonstate.edu>
2021-05-31 19:15:37 -0700

    Implemented testing for leap year div by 400

--- leapyear_test.py
+++ leapyear_test.py
@@ -5,13 +5,17 @@ class TestLeapYear(unittest.Testcase):
 	
 	def test_leap_div_4(self):
 		self.assertTrue(LeapCalc(4))
-		self.assertTrue(LeapCalc(2000))
+		self.assertTrue(LeapCalc(1600))
 		self.assertTrue(LeapCalc(2020))
 
 	def test_not_leap_div_100(self):
 		self.assertFalse(LeapCalc(1800))
 		self.assertFalse(LeapCalc(1900))
 		self.assertFalse(LeapCalc(2500))
+
+	def test_leap_div_400(self):
+		self.assertTrue(LeapCalc(2000))
+		self.assertTrue(LeapCalc(2800))
 
 if __name__ == '__main__':
 	unittest.main()
