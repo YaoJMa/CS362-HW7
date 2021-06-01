e786726935b22be97ad6da98f4b924943d9e4a09
Yao Ma <mayao@flip1.engr.oregonstate.edu>
2021-05-31 18:55:27 -0700

    Implemented printing fizzbuzz w/ mult of 3 and 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,14 @@
 def FizzBuzz(num=100):
 	for i in range(num):
-		if(i%3==0):
+		if(i%3==0 and i%5==0):
+			print("fizzbuzz")
+    
+		elif(i%3==0):
 			print("fizz")
 		
 		elif(i%5==0):
 			print("buzz")
+
 		else: 
 			print(i)
 FizzBuzz()
