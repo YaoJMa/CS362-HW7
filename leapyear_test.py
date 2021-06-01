567b027ace7abf9f94c33e1eda2c7b5d76a8ea87
Yao Ma <mayao@flip1.engr.oregonstate.edu>
2021-05-31 19:13:39 -0700

    Implemented testing for not leap ytear if divisible by 100

--- leapyear_test.py
+++ leapyear_test.py
@@ -8,5 +8,10 @@ class TestLeapYear(unittest.Testcase):
 		self.assertTrue(LeapCalc(2000))
 		self.assertTrue(LeapCalc(2020))
 
+	def test_not_leap_div_100(self):
+		self.assertFalse(LeapCalc(1800))
+		self.assertFalse(LeapCalc(1900))
+		self.assertFalse(LeapCalc(2500))
+
 if __name__ == '__main__':
 	unittest.main()
